7606f74863a04c184008aff28081a3d65d7b5940
Lamar Retemyer <lamar.retemyer@gmail.com>
2024-02-05 18:08:36 -0400

    Completed Question 1

---
+++ JavaMCQ.java
@@ -0,0 +1,17 @@
+public class JavaMCQ {
+    public static void main(String[] args) {
+        final String constString = "TRUE";
+        final String const1String = "FALSE";
+
+        System.out.println("(a) Reserved words in Java can be redefined by the programmer to mean something other than their original intentions. "+ constString);
+        System.out.println("(b) In a Java program, dividing by zero is a syntax error. " + const1String);
+        System.out.println("(c) Java byte codes are directly executable whereas Java source code is not. " + constString);
+        System.out.println("(d) The Java compiler is able to find all programmer errors. " + const1String);
+        System.out.println("(e) Java is a case-sensitive language which means Current, CURRENT, and current will all reference the same identifier. " + const1String);
+    }
+    
+}
+
+
+
+
